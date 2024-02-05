6981bb9e76c56cb27a6f5eb11fe6986d430ce98a
theofeur <154354851+theofeur@users.noreply.github.com>
2024-02-05 20:24:31 +0100

    Correction bug doublons

--- GO/Tests/vomir.go
+++ GO/Tests/vomir.go
@@ -11,6 +11,11 @@ import (
 
 const port = ":8080"
 
+var (
+	registeredPaths = make(map[string]bool)
+	mu              sync.Mutex
+)
+
 func Home(w http.ResponseWriter, r *http.Request, body string) {
 	fmt.Fprintf(w, body)
 }
@@ -19,8 +24,14 @@ func Lire(filePath string, wg_poster *sync.WaitGroup) {
 
 	defer wg_poster.Done()
 
-	content, _ := os.ReadFile(filePath)
+	content, err := os.ReadFile(filePath)
+	if err != nil {
+		fmt.Printf("Erreur lors de la lecture du fichier %s : %v\n", filePath, err)
+		return
+	}
 	fileContent := string(content)
+	mu.Lock()
+	defer mu.Unlock()
 
 	// Trouver la position de la séquence "corps_du_texte"
 	index := strings.Index(fileContent, "\ncorps_du_texte\n")
@@ -32,9 +43,15 @@ func Lire(filePath string, wg_poster *sync.WaitGroup) {
 
 	// Extraire ce qui se trouve avant et après la séquence
 	titre := fileContent[:index]
+	if _, exists := registeredPaths[titre]; exists {
+		fmt.Printf("Le lien %s est déjà enregistré.\n", titre)
+		return
+	}
+	registeredPaths[titre] = true
 	body := fileContent[index+len("\ncorps_du_texte\n"):]
-
 	http.HandleFunc(titre, func(w http.ResponseWriter, r *http.Request) { poster(w, r, body) })
+	fmt.Printf("Enregistrement du lien : localhost:8080%s\n", titre)
+
 }
 
 func poster(w http.ResponseWriter, r *http.Request, body string) {
@@ -44,7 +61,7 @@ func poster(w http.ResponseWriter, r *http.Request, body string) {
 func main() {
 
 	// chemin de votre fichier
-	filePath := "HTML/Test0"
+	filePath := "HTML0/Page0"
 	// Lecture du fichier
 	content, err := os.ReadFile(filePath)
 	if err != nil {
@@ -63,26 +80,34 @@ func main() {
 	}
 
 	// Extraire ce qui se trouve avant et après la séquence
+
 	titre := fileContent[:index]
 	body := fileContent[index+len("\ncorps_du_texte\n"):]
 
 	http.HandleFunc(titre, func(w http.ResponseWriter, r *http.Request) { Home(w, r, body) })
+	registeredPaths[titre] = true
 
 	fmt.Printf("Le lien : localhost:8080%s \n", titre)
 	fmt.Printf("Serveur écoutant sur le port %s...\n", port)
 
 	startTime := time.Now()
 
-	infos, err := os.ReadDir("HTML")
-	nb_fichiers := len(infos)
-
 	var wg_poster sync.WaitGroup
+	profondeur := 5
+	for j := 0; j < profondeur; j++ {
+		lien_profondeur := fmt.Sprintf("HTML%d", j)
+		infos, err := os.ReadDir(lien_profondeur)
+		if err != nil {
+			fmt.Printf("Erreur lors de la lecture du répertoire %s : %v\n", lien_profondeur, err)
+			continue
+		}
+		nb_fichiers := len(infos)
+		for i := 1; i < nb_fichiers; i++ {
 
-	for i := 1; i < nb_fichiers; i++ {
+			wg_poster.Add(1)
+			Lire(fmt.Sprintf("HTML%d/Page%d", j, i), &wg_poster)
 
-		wg_poster.Add(1)
-		Lire(fmt.Sprintf("HTML/Test%d", i), &wg_poster)
-
+		}
 	}
 
 	wg_poster.Wait()
